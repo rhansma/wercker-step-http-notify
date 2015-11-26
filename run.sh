c0d02f407003a854041f1a4fc98a55ecd222eada
Robin <robinhansma@outlook.com>
2015-11-26 10:30:02 +0100

    Conditionally send headers if defined

--- run.sh
+++ run.sh
@@ -6,12 +6,24 @@ else
   export WERCKER_HTTP_NOTIFY_TYPE="deploy"
 fi
 
-curl if [ -z "$WERCKER_HTTP_NOTIFY_HEADER" ]; then echo "-H $WERCKER_HTTP_NOTIFY_HEADER \"; else echo "\\"; fi
-     -d "result=$WERCKER_RESULT" \
-     -d "type=$WERCKER_HTTP_NOTIFY_TYPE" \
-     -d "application_owner_name=$WERCKER_APPLICATION_OWNER_NAME" \
-     -d "application_name=$WERCKER_APPLICATION_NAME" \
-     -d "git_branch=$WERCKER_GIT_BRANCH" \
-     -d "started_by=$WERCKER_STARTED_BY" \
-     -d "build_url=$WERCKER_BUILD_URL" \
-     $WERCKER_HTTP_NOTIFY_URL
+# If headers defined send them, else dont send headers
+if [ -z "$WERCKER_HTTP_NOTIFY_HEADER" ]; then
+  curl -H $WERCKER_HTTP_NOTIFY_HEADER \
+       -d "result=$WERCKER_RESULT" \
+       -d "type=$WERCKER_HTTP_NOTIFY_TYPE" \
+       -d "application_owner_name=$WERCKER_APPLICATION_OWNER_NAME" \
+       -d "application_name=$WERCKER_APPLICATION_NAME" \
+       -d "git_branch=$WERCKER_GIT_BRANCH" \
+       -d "started_by=$WERCKER_STARTED_BY" \
+       -d "build_url=$WERCKER_BUILD_URL" \
+       $WERCKER_HTTP_NOTIFY_URL
+else 
+curl   -d "result=$WERCKER_RESULT" \
+       -d "type=$WERCKER_HTTP_NOTIFY_TYPE" \
+       -d "application_owner_name=$WERCKER_APPLICATION_OWNER_NAME" \
+       -d "application_name=$WERCKER_APPLICATION_NAME" \
+       -d "git_branch=$WERCKER_GIT_BRANCH" \
+       -d "started_by=$WERCKER_STARTED_BY" \
+       -d "build_url=$WERCKER_BUILD_URL" \
+       $WERCKER_HTTP_NOTIFY_URL
+fi
