8afb78f2c03169438efb13d740d573d987b9c346
Robin <robinhansma@outlook.com>
2015-11-26 09:41:19 +0100

    Update run.sh
    Made headers optional

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ else
   export WERCKER_HTTP_NOTIFY_TYPE="deploy"
 fi
 
-curl -H $WERCKER_HTTP_NOTIFY_HEADER \
+curl if [ -z "$WERCKER_HTTP_NOTIFY_HEADER" ]; then echo "-H $WERCKER_HTTP_NOTIFY_HEADER \"; else echo "\\"; fi
      -d "result=$WERCKER_RESULT" \
      -d "type=$WERCKER_HTTP_NOTIFY_TYPE" \
      -d "application_owner_name=$WERCKER_APPLICATION_OWNER_NAME" \
