8da149a95d616c4347f70459ba37b2d3cb54e1ed
Robin <robinhansma@outlook.com>
2015-11-26 10:48:00 +0100

    Make request method configurable

--- run.sh
+++ run.sh
@@ -6,9 +6,15 @@ else
   export WERCKER_HTTP_NOTIFY_TYPE="deploy"
 fi
 
+# Default to request method to GET
+if [ ! -z "$WERCKER_HTTP_NOTIFY_METHOD" ]; then
+  $WERCKER_HTTP_NOTIFY_METHOD="GET"
+fi
+
 # If headers defined send them, else dont send headers
 if [ -z "$WERCKER_HTTP_NOTIFY_HEADER" ]; then
-  curl -H $WERCKER_HTTP_NOTIFY_HEADER \
+  curl -X $WERKCER_HTTP_NOTIFY_METHOD \
+       -H $WERCKER_HTTP_NOTIFY_HEADER \
        -d "result=$WERCKER_RESULT" \
        -d "type=$WERCKER_HTTP_NOTIFY_TYPE" \
        -d "application_owner_name=$WERCKER_APPLICATION_OWNER_NAME" \
@@ -18,7 +24,8 @@ if [ -z "$WERCKER_HTTP_NOTIFY_HEADER" ]; then
        -d "build_url=$WERCKER_BUILD_URL" \
        $WERCKER_HTTP_NOTIFY_URL
 else 
-curl   -d "result=$WERCKER_RESULT" \
+curl   -X $WERKCER_HTTP_NOTIFY_METHOD \
+       -d "result=$WERCKER_RESULT" \
        -d "type=$WERCKER_HTTP_NOTIFY_TYPE" \
        -d "application_owner_name=$WERCKER_APPLICATION_OWNER_NAME" \
        -d "application_name=$WERCKER_APPLICATION_NAME" \
